371c06f90d5d9dcc456c46474b1c147ea8b9fa83
Jessé Sampaio <jessesampaio.ti@gmail.com>
2018-09-10 22:09:20 -0300

    Projeto criado

---
+++ src/main/java/br/com/enterprise/cursomc/CursoSpringBootModeloConceitualApplication.java
@@ -0,0 +1,12 @@
+package br.com.enterprise.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBootModeloConceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBootModeloConceitualApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/enterprise/cursomc/CursoSpringBootModeloConceitualApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.enterprise.cursomc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursoSpringBootModeloConceitualApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
